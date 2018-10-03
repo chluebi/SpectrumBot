d7e796efa3beb2f6c621e0091f8d7343b40f71df
Luis Maltsis <ninjago@sunrise.ch>
2018-10-03 18:39:43 +0200

    first version

--- spectrumbot.py
+++ spectrumbot.py
@@ -3,13 +3,41 @@ import json
 
 client = discord.Client()
 
-with open('/home/chluebi/Documents/PythonFiles/SpectrumBot/tokens.json') as f:
+with open('/home/chluebi/Documents/PythonFiles/SpectrumBotv0.0.1/tokens.json') as f:
     tokens = json.load(f)
 
+with open('/home/chluebi/Documents/PythonFiles/SpectrumBotv0.0.1/roles.txt') as f:
+    roles = f.read()
+
+print(roles)
+
+
 discordtoken = tokens['discord']['token']
+prefix = '$$'
+
+def parse(msg):  # parses the received message for better handling
+    try:
+        return msg.split()
+    except TypeError:
+        pass
+
+@client.event
+async def on_ready():
+    print('Logged in as ' + client.user.name)
+    server = client.get_guild(496658459354464276)
+    print('Found server: ' + server.name)
 
 
-print(discordtoken)
+@client.event
+async def on_message(message):
+    msg = parse(message.content[len(prefix):])
+
+    if message.content.startswith(prefix):
+        if msg[0] == 'roles':
+            await message.channel.send(roles)
+
+
+
 
 
 client.run(discordtoken)
